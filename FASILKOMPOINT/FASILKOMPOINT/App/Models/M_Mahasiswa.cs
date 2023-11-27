b2498d1c15634026e7f464212197baa3a96e66ac
Valentino Hariyanto <117331875+valentinohrynt@users.noreply.github.com>
2023-11-27 21:09:01 +0700

    Update M_Mahasiswa.cs

--- FASILKOMPOINT/FASILKOMPOINT/App/Models/M_Mahasiswa.cs
+++ FASILKOMPOINT/FASILKOMPOINT/App/Models/M_Mahasiswa.cs
@@ -11,7 +11,7 @@ namespace FASILKOMPOINT.App.Models
     internal class M_Mahasiswa
     {
         [Key]
-        public string username { get; set; }
+        public int username { get; set; }
         [Required]
         public string nama { get; set; }
         [ForeignKey("M_Prodi")]
