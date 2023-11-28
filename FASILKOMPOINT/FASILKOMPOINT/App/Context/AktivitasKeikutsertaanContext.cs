593e9189921f732cedcf55a66f11aee040dee273
valentinohrynt <hariyantovalentino@gmail.com>
2023-11-28 18:05:50 +0700

    Mengubah query untuk semua Aktivitas Context agar terintegrasi dengan Tabel Komentar ketika menambah dan mengubah data pada fitur entry data SKPI aktor mahasiswa

--- FASILKOMPOINT/FASILKOMPOINT/App/Context/AktivitasKeikutsertaanContext.cs
+++ FASILKOMPOINT/FASILKOMPOINT/App/Context/AktivitasKeikutsertaanContext.cs
@@ -30,7 +30,7 @@ namespace FASILKOMPOINT.App.Context
         }
         public static void addKeikutsertaan(M_Aktivitas_Keikutsertaan aktivitasKeikutsertaanBaru)
         {
-            string query = $"INSERT INTO {table}(id_aktivitas, nama_kegiatan, tanggal, file_bukti, jenis_kepesertaan, level_tingkat, poin_id_poin, mahasiswa_username, is_acc, kategori_id_kategori) SELECT COALESCE(MAX(id_aktivitas), 0) + 1, @nama_kegiatan, @tanggal, @file_bukti, @jenis_kepesertaan, @level_tingkat, (SELECT id_poin FROM poin WHERE butir_id_butir = (SELECT id_butir FROM butir WHERE nama_butir = @level_tingkat) AND subbutir_id_subbutir = (SELECT id_subbutir FROM subbutir WHERE nama_subbutir = @jenis_kepesertaan) AND kategori_id_kategori = @kategori), @mahasiswa_username, @is_acc, @kategori FROM {table};";
+            string query = $"DO $$ DECLARE new_id_aktivitas INTEGER; BEGIN INSERT INTO {table} (id_aktivitas, nama_kegiatan, tanggal, file_bukti, jenis_kepesertaan, level_tingkat, poin_id_poin, mahasiswa_username, is_acc, kategori_id_kategori) VALUES (COALESCE((SELECT MAX(id_aktivitas) FROM {table}), 0)+1, @nama_kegiatan, @tanggal, @file_bukti, @jenis_kepesertaan, @level_tingkat, (SELECT id_poin FROM poin WHERE butir_id_butir = (SELECT id_butir FROM butir WHERE nama_butir = @level_tingkat) AND subbutir_id_subbutir = (SELECT id_subbutir FROM subbutir WHERE nama_butir = @jenis_kepesertaan) AND kategori_id_kategori = @kategori), @mahasiswa_username, @is_acc, @kategori) RETURNING id_aktivitas INTO new_id_aktivitas; INSERT INTO komentar (id_komentar, aktivitas_id_aktivitas, komentar, tim_skpi_username) VALUES (COALESCE((SELECT MAX(id_komentar) FROM komentar), 0) + 10, new_id_aktivitas, 'Belum ada komentar', '198706192014041001'); END $$;\r\n";
             NpgsqlParameter[] parameters =
             {
                 new NpgsqlParameter("@nama_kegiatan", NpgsqlDbType.Varchar){Value = aktivitasKeikutsertaanBaru.nama_kegiatan},
@@ -44,9 +44,9 @@ namespace FASILKOMPOINT.App.Context
             };
             commandExecutor(query, parameters);
         }
-        public static void updatePrestasi(M_Aktivitas_Keikutsertaan aktivitasKeikutsertaanEdit)
+        public static void updateKeikutsertaan(M_Aktivitas_Keikutsertaan aktivitasKeikutsertaanEdit)
         {
-            string query = $"UPDATE {table} SET nama_kegiatan = @nama_kegiatan, tanggal = @tanggal, file_bukti = @file_bukti, jenis_kepesertaan = @jenis_kepesertaan, level_tingkat = @level_tingkat, poin_id_poin = (SELECT id_poin FROM poin WHERE butir_id_butir = (SELECT id_butir FROM butir WHERE nama_butir = @tingkat_penyelenggara) AND kategori_id_kategori = @kategori), is_acc = @is_acc WHERE id_aktivitas = @id_aktivitas";
+            string query = $"DO $$ DECLARE new_id_aktivitas INTEGER; BEGIN UPDATE {table} SET nama_kegiatan = @nama_kegiatan, tanggal = @tanggal, file_bukti = @file_bukti, jenis_kepesertaan = @jenis_kepesertaan, level_tingkat = @level_tingkat, poin_id_poin = (SELECT id_poin FROM poin WHERE butir_id_butir = (SELECT id_butir FROM butir WHERE nama_butir = @tingkat_penyelenggara) AND kategori_id_kategori = @kategori), is_acc = @is_acc WHERE id_aktivitas = @id_aktivitas RETURNING id_aktivitas INTO new_id_aktivitas; UPDATE komentar SET komentar = 'Belum ada komentar', tim_skpi_username = '198706192014041001' WHERE aktivitas_id_aktivitas = new_id_aktivitas; END $$;\r\n";
             NpgsqlParameter[] parameters =
             {
                 new NpgsqlParameter("@nama_kegiatan", NpgsqlDbType.Varchar){Value = aktivitasKeikutsertaanEdit.nama_kegiatan},
@@ -62,7 +62,7 @@ namespace FASILKOMPOINT.App.Context
         }
         public DataTable showRekapKeikutsertaan(string username, int kategori)
         {
-            string query = $"SELECT {table}.nama_kegiatan as Kegiatan, {table}.tanggal as Tanggal, {table}.file_bukti as Bukti, {table}.jenis_kepesertaan as Kepesertaan, {table}.level_tingkat as Tingkat, poin.poin as Poin FROM {table} JOIN poin ON poin.id_poin = {table}.poin_id_poin WHERE {table}.mahasiswa_username = @username and {table}.kategori_id_kategori = @kategori and {table}.is_acc = 'disetujui';";
+            string query = $"SELECT ROW_NUMBER() OVER (ORDER BY {table}.tanggal) as No, {table}.nama_kegiatan as Nama, CONCAT('Tanggal: ', {table}.tanggal, '\r\n', 'Tingkat: ', {table}.level_tingkat, '\r\n', 'Jenis: ', {table}.jenis_kepesertaan) as Keterangan, {table}.file_bukti as Bukti,{table}.is_acc as Status, poin.poin as Poin FROM {table} JOIN poin ON poin.id_poin = {table}.poin_id_poin WHERE {table}.mahasiswa_username = @username AND {table}.kategori_id_kategori = @kategori AND {table}.is_acc = 'disetujui';";
             NpgsqlParameter[] parameters =
             {
                 new NpgsqlParameter("@kategori", NpgsqlDbType.Integer){Value = kategori},
