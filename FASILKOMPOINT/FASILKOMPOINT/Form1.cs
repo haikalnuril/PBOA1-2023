4a03ff1f2af0811e8e57a547e2986d16de93f29d
valentinohrynt <hariyantovalentino@gmail.com>
2023-11-29 12:30:56 +0700

    Menambahkan beberapa line ke Form1.cs

--- FASILKOMPOINT/FASILKOMPOINT/Form1.cs
+++ FASILKOMPOINT/FASILKOMPOINT/Form1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -13,15 +14,61 @@ namespace FASILKOMPOINT
 {
     public partial class Form1 : Form
     {
+        public string username;
         public Form1()
         {
             InitializeComponent();
-            TabelData.DataSource = MahasiswaContext.showDataSKPIMahasiswa();
+            TabelData.DataSource = AktivitasPrestasiContext.showPrestasi("222410101001", 602);
+
+            TabelData.Columns["Keterangan"].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
+        private void TabelData_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        //private void SearchNama_KeyPress(object sender, KeyPressEventArgs e)
+        //{
+        //    if (e.KeyChar == (char)Keys.Enter)
+        //    {
+        //        ExecuteSearch();
+        //        e.Handled = true;
+        //    }
+        //}
+        //        private void ExecuteSearch()
+        //        {
+        //            string nama = SearchNama.Text;
+        //            try
+        //            {
+        //                if (string.IsNullOrWhiteSpace(nama))
+        //                {
+        //                    tabelDataSKPIMahasiswa.DataSource = MahasiswaContext.showDataSKPIMahasiswa();
+        //                }
+        //                else
+        //                {
+        //                    tabelDataSKPIMahasiswa.DataSource = MahasiswaContext.showSearchDataSKPIMahasiswa(nama);
+        //                }
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show("Error: " + ex, "Kesalahan", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        //            }
+        //        }
+        //        private void SearchNama_TextChanged(object sender, EventArgs e)
+        //        {
+        //            if (string.IsNullOrWhiteSpace(SearchNama.Text))
+        //            {
+        //                tabelDataSKPIMahasiswa.DataSource = MahasiswaContext.showDataSKPIMahasiswa();
+        //            }
+        //        }
+
+        //        private void ButtonSearch_Click(object sender, EventArgs e)
+        //        {
+        //            ExecuteSearch();
+        //        }
     }
 }
